7c16b349d0fa1cc0e91d28881eeeb5f22b753ea3
Viktoriia-Bob <83957091+Viktoriia-Bob@users.noreply.github.com>
2021-06-21 09:58:56 +0300

    Delete tasks2 directory

--- tasks2/task2.js
+++
@@ -1,8 +0,0 @@
-function convertStringToArray(string) {
-    return string.split(/\W/);
-}
-
-const str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-const arr = convertStringToArray(str);
-
-console.log(arr);
--- tasks2/task3.js
+++
@@ -1,3 +0,0 @@
-let str = 'I-know-javascript!';
-str = str.replace(/./g, '!');
-console.log(str);

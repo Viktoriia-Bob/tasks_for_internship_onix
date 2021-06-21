0bc6b1b0ccaf2738128bd741544a18cb7caeb2a5
Viktoriia-Bob <83957091+Viktoriia-Bob@users.noreply.github.com>
2021-06-21 09:58:45 +0300

    Delete tasks1 directory

--- tasks1/task2.js
+++
@@ -1,5 +0,0 @@
-let st = 0;
-for (let i = 2; i <= 10; i++) {
-    st += i;
-}
-console.log(st);
--- tasks1/task3.js
+++
@@ -1,3 +0,0 @@
-const getRandomInt = () => Math.floor(1 + Math.random() * 100);
-
-console.log(getRandomInt());
